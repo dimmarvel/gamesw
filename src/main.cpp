9f7d0036a98004804e4d3ed902647ae267043ae6
Dmitry Matsiukhov <dimmarvel@mail.ru>
2024-11-08 23:42:05 +0300

    [DEV] Delete unused include

--- src/main.cpp
+++ src/main.cpp
@@ -1,23 +1,7 @@
 #include <Engine/Core/GameObserver.hpp>
-#include <Engine/Core/Map.hpp>
 #include <Engine/Core/Settings.hpp>
 #include <Engine/Renderer/TextRenderer.hpp>
-#include <IO/Commands/CreateMap.hpp>
-#include <IO/Commands/March.hpp>
-#include <IO/Commands/SpawnHunter.hpp>
-#include <IO/Commands/SpawnSwordsman.hpp>
-#include <IO/Events/MapCreated.hpp>
-#include <IO/Events/MarchEnded.hpp>
-#include <IO/Events/MarchStarted.hpp>
-#include <IO/Events/UnitAttacked.hpp>
-#include <IO/Events/UnitDied.hpp>
-#include <IO/Events/UnitMoved.hpp>
-#include <IO/Events/UnitSpawned.hpp>
-#include <IO/System/CommandParser.hpp>
-#include <IO/System/EventLog.hpp>
-#include <IO/System/PrintDebug.hpp>
 #include <fstream>
-#include <iostream>
 
 int main(int argc, char** argv)
 {
